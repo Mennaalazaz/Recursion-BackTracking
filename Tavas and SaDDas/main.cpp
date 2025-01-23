bd0c05ab54c7830c5a76a0ba9d06b8ac493a2a32
Menna <162923696+Mennaalazaz@users.noreply.github.com>
2025-01-23 16:09:30 +0200

    Update main.cpp

--- Tavas and SaDDas/main.cpp
+++ Tavas and SaDDas/main.cpp
@@ -24,6 +24,20 @@ void lucky(int num=0, int idx=0) {
     lucky(num*10+4, 2*idx+1);
     lucky(num*10+7, 2*idx+2);
 }
+int lucky2(int num=0, int idx=0) {
+    // base case
+    if (num > target) {
+        return INT_MIN;
+    }
+    if (num == target) {
+        return idx;
+    }
+
+    // transition
+    int left=lucky(num*10+4, 2*idx+1);
+    int right=lucky(num*10+7, 2*idx+2);
+    return max(left, right); // to ensure that the value returned is the idx
+}
 
 void solve() {
     cin >> target;
